7c41156f3c42710e981035d42f7557f469076ce5
Bobby Smith <bobby.smith@gmail.com>
2017-07-25 12:18:00 -0400

    Pulse: Committing a Generated Stub

---
+++ features/step_definitions/the_shop_should_be_closed_on_tuesday.rb
@@ -0,0 +1,18 @@
+
+require "spec_helper"
+
+Given(/^a menu order$/) do
+  pending
+end
+
+When(/^the date is Tuesday$/) do
+  pending
+end
+
+Then(/^I expect the inventory count to equal previous count$/) do
+  fail
+end
+
+Then(/^I expect a message 'coffees are not sold on Tuesdays'/) do
+  pending
+end
