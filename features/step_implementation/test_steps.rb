ecff429a44f4515d4343ca563e170468514270e2
denodaeus <bobby.smith@gmail.com>
2017-08-29 22:28:40 -0700

    test steps

---
+++ features/step_implementation/test_steps.rb
@@ -0,0 +1,17 @@
+# CS-1
+
+Given /^an inventory list of 3 coffees$/ do
+  expect(true).to eql(true)
+endate
+
+When /^I purchase a coffee$/ do
+  expect(true).to eql(true)
+end
+
+When /^I receive it$/ do
+  expect(true).to eql(true)
+end
+
+Then /^I expect the inventory count to equal 2$/ do
+  expect(true).to eql(true)
+end
