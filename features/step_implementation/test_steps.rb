76dd4995a3937805b287e307c690e49fc156b12e
denodaeus <bobby.smith@gmail.com>
2017-08-29 22:32:25 -0700

    fix tests

--- features/step_implementation/test_steps.rb
+++ features/step_implementation/test_steps.rb
@@ -1,5 +1,6 @@
 # CS-1
 
+# Scenario-1
 Given /^an inventory list of 3 coffees$/ do
   expect(true).to eql(true)
 end
@@ -13,5 +14,18 @@ When /^I receive it$/ do
 end
 
 Then /^I expect the inventory count to equal 2$/ do
+  expect(true).to eql(true)
+end
+
+# Scenario-2
+Given /^an inventory list of 1 coffees$/ do
+  expect(true).to eql(true)
+end
+
+Then /^I expect the inventory count to equal 0$/ do
+  expect(true).to eql(true)
+end
+
+Then /^I expect coffees to be marked unavailable$/ do
   expect(true).to eql(true)
 end
