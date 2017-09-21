8e72db2248012d826ced435841858f078554901c
Bobby Smith <bobby.smith@gmail.com>
2017-09-21 15:40:20 -0400

    Update the_shop_should_be_closed_on_tuesdays.rb

--- features/step_definitions/the_shop_should_be_closed_on_tuesdays.rb
+++ features/step_definitions/the_shop_should_be_closed_on_tuesdays.rb
@@ -1,9 +1,3 @@
-When /^the date is Tuesday$/ do
-  expect(true).to eql(true)
-end
-Then /^I expect the inventory count to equal previous count$/ do
-  expect(true).to eql(false)
-end
 And /^I expect a message 'coffees are not sold on Tuesdays'$/ do
   expect(true).to eql(false)
   # pending
