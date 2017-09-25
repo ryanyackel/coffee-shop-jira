a24dd39317cb47350b799615c81cd7e9f5d45154
Bobby Smith <bobby.smith@gmail.com>
2017-09-25 13:03:40 -0400

    Pulse: Committing a Generated Stub

---
+++ features/step_definitions/the_shop_should_be_closed_on_tuesdays.rb
@@ -0,0 +1,15 @@
+
+Given /^a menu order$/ do
+  expect(true).to eql(true)
+end
+When /^the date is Tuesday$/ do
+  expect(true).to eql(true)
+end
+Then /^I expect the inventory count to equal previous count$/ do
+  expect(true).to eql(false)
+end
+And /^I expect a message 'coffees are not sold on Tuesdays'$/ do
+  expect(true).to eql(false)
+  # pending
+end
+  
